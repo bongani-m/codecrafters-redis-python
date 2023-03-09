9f7156f1ebe61d82484d1d726d85e1d4ff76359f
Bongani Mbigi <bongani.mbigi@icloud.com>
2023-03-09 07:55:31 -0600

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -9,7 +9,18 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            if data is not None:
+                conn.sendall(b"+PONG\r\n")
+                data = None
+            
+
 
 
 if __name__ == "__main__":
